bed1bbc9592552b8b961c703fdb57f776a9bb196
Jeffrey Jordan <jeff2016@alumni.stanford.edu>
2017-10-13 11:34:14 -0700

    completes int_set

--- my_hash_map/lib/p01_int_set.rb
+++ my_hash_map/lib/p01_int_set.rb
@@ -1,19 +1,25 @@
 class MaxIntSet
   def initialize(max)
+    @store = Array.new(max) {false}
   end
 
   def insert(num)
+    raise "Out of bounds" if is_valid?(num)
+    @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+     num < 0 || num > @store.length
   end
 
   def validate!(num)
@@ -27,12 +33,18 @@ class IntSet
   end
 
   def insert(num)
+    idx = num%20
+    @store[idx] << num
   end
 
   def remove(num)
+    idx = num%20
+    @store[idx].delete(num)
   end
 
   def include?(num)
+    idx = num%20
+    @store[idx].include?(num)
   end
 
   private
@@ -55,12 +67,28 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if (@count+1>=@store.length)
+      resize!
+    end
+    idx = num % @store.length
+    if !@store[idx].include?(num)
+      @store[idx] << num
+      @count += 1
+    end
   end
 
   def remove(num)
+
+    idx = num % @store.length
+    if @store[idx].include?(num)
+      @store[idx].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    idx = num % @store.length
+    @store[idx].include?(num)
   end
 
   private
@@ -74,5 +102,13 @@ class ResizingIntSet
   end
 
   def resize!
+    new_store = Array.new(@store.length*2) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |item|
+        idx = item%new_store.length
+        new_store[idx] << item
+      end
+    end
+    @store = new_store
   end
 end
